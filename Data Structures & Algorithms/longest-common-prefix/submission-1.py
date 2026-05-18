04807892b301931898a9d57a868d31b961558db0
Sean Wilder <53lemonz@gmail.com>
2026-05-18 14:50:57 -0400

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.py
@@ -0,0 +1,34 @@
+'''
+Observations:
+need to look at everything, 
+if a letter conflicts thats not the first one: stop and return the current string.
+
+
+Thoughts:
+sliding window? 
+but then have to eval against every other item
+
+Conclusion:
+res = ""
+Since i return an empty array if nothing matches, i can point at one letter,
+check if all letters in the same position match for all str,
+then if they dont, delete the previous letter if not the first index,
+return res
+if letters match:
+    res.append(currently indexed letter)
+
+Feel like we could do this recursively, revisit later
+'''
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = strs[0]
+        for word in range(len(strs)):
+            letter = 0
+            while letter < min(len(res), len(strs[word])):
+                if res[letter] != strs[word][letter]:
+                    break
+                letter +=1
+            res = res[:letter]
+        return res
