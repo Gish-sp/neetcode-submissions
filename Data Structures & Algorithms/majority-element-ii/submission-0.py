0ae7ca56899999e519f336e969bde8992eb56c5e
Sean Wilder <53lemonz@gmail.com>
2026-05-21 14:26:09 -0400

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,14 @@
+'''
+Idea:
+1: count the amount of times any element appears in the list
+    if it occur more than len(nums) // 3, add it to a list res, return res
+'''
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        counted = Counter(nums)
+        res = []
+        for val in counted:
+            if counted[val]  > len(nums) // 3:
+                res.append(val)
+        return res
